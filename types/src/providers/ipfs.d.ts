fed9f01d7089869f4dd91b606144120ad1ea16f1
Adam Uhlíř <adam@uhlir.dev>
2019-12-25 13:00:55 +0100

    revert: chore: tmp lib add
    This reverts commit 174c2a3655e562eb422fb687d2bc0af511c1120d.

--- types/src/providers/ipfs.d.ts
+++
@@ -1,9 +0,0 @@
-import { IpfsStorageProvider } from '../types';
-import { ClientOptions, IpfsClient } from 'ipfs-http-client';
-/**
- * Factory for supporting IPFS
- *
- * @param options
- * @constructor
- */
-export default function IpfsFactory(options: ClientOptions | IpfsClient): IpfsStorageProvider;
